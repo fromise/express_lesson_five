fa85c18897e391c1fbda79e3e2eba8842f54bbdb
fromise <cddar577@gmail.com>
2023-10-25 08:03:35 -0700

    Learned how to post to a database in Sequelize.

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,12 @@ var express = require('express');
 var router = express.Router();
 var mysql = require('mysql2');
 var models = require('../models');
+/* 
+For complex operations, Sequelize and op need to be defined.
+use an op.and or op.or to specify multiple complex requests.
+*/
+var Sequelize = require('sequelize');
+var op = Sequelize.Op;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -30,11 +36,11 @@ router.get('/specificActor', function(req, res, next) {
 
 
 router.get('/actors', function(req, res, next) {
-models.actor.findAll({}).then(actorsFound => {
-  res.render('actors', {
-    actors: actorsFound
+  models.actor.findAll({}).then(actorsFound => {
+    res.render('actors', {
+      actors: actorsFound
+    });
   });
-})
 });
 
 router.get('/actor/:id', function(req, res, next) {
@@ -49,6 +55,23 @@ res.render('specificActor', {
   actor: actor
 })
 });
+});
+
+router.post('/actor', (req, res) => {
+  models.actor
+    .findOrCreate({
+      where: {
+        first_name: req.body.first_name,
+        last_name: req.body.last_name
+      }
+    })
+    .spread(function(result, created) {
+      if (created) {
+        res.redirect('/actors');
+      } else {
+        res.send('This actor already exists!');
+      }
+    });
 });
 
 module.exports = router;
