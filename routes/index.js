cf8fd11cb1a104d73386564aeef13af2a785583f
fromise <cddar577@gmail.com>
2023-10-25 06:57:14 -0700

    Learned how to use findOne() in Sequelize.

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,40 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* The router function calls for something to be done using a specific view.
+models.actor provides the list of actors I'm pulling from.
+where: specifies which actors information is wanted.
+Then, I use an arrow function to render the actor.
+actor: actor tells refers to the actor.first_name + actor.last_name commands found in the view.
+*/
+router.get('/specificActor', function(req, res, next) {
+  models.actor
+    .findOne({
+      where: {
+        actor_id: 2
+      }
+    })
+    .then(actor => {
+      res.render('specificActor', {
+        actor: actor
+      });
+    });
+});
+
+router.get('/NickWahlberg', function(req, res, next) {
+  models.actor
+    .findOne({
+      where: {
+        actor_id: 2
+      }
+    })
+    .then(actor => {
+      res.render('NickWahlberg', {
+        actor: actor
+      });
+    });
+});
+
 router.get('/actors', function(req, res, next) {
 models.actor.findAll({}).then(actorsFound => {
   res.render('actors', {
